b31b3ee9060d6709f5307e727eb99932779d1b92
shimUnBo <pencer118@gmail.com>
2024-01-12 23:39:02 +0900

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const[value, setValue] = useState("");
+
+  const btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    cursor: "pointer",
+    float:"right",
+  };
+
+  const getStyle =(completed) => {
+    return {
+      padding : "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
+
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    console.log("newTodoData",newTodoData);
+    setTodoData(newTodoData);
+
+  };
+  const handleChange =(e)=>{
+    setValue(e.target.value);
+  };
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+    setTodoData((prev) => [...prev,newTodo]);
+    setValue("");
+  }
+
+  const handleCompleteChane =(id) =>{
+    let newTodoData = todoData.map((data) => {
+      if(data.id === id){
+        data.completed =! data.completed;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+  };
+
+
+    return(
+      <div className = "body">
+        <div className="title"><h1>예산 계산기</h1></div>
+      <div className = "container">
+        <div className="todoBlock">
+          {todoData.map((data) => (
+            <div style={getStyle(data.completed)} key={data.id}>
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={()=>handleCompleteChane(data.id)}
+              />
+              {data.title}
+              <button
+                style={btnStyle}
+                onClick={()=>handleClick(data.id)}
+              >
+              X
+              </button>
+              </div>
+          ))}
+          <form style={{display:"flex" }} onSubmit={handleSubmit}>
+            <div className="list">지출항목</div>
+            <input
+              type="text"
+              name="value"
+              style={{flex:"10", padding:"5px"}}
+              placeholder="예) 렌트비"
+              value={value}
+              onChange={handleChange}
+              />
+              <div className="price">비용</div>
+              <input
+              type="text"
+              name="price"
+              style={{flex:"10", padding:"5px"}}
+              placeholder="0"
+              value={value}
+              onChange={handleChange}
+              />
+              <input
+                type="submit"
+                value="입력"
+                className="btn"
+                style={{flex:"1"}}
+                />
+          </form>
+        </div>
+        </div>
+        </div>
+    );
 }
-
-export default App;
